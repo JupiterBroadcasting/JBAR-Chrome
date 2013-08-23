7cf624dd617aae8f71b37ab43741d9c60cd79cef
Erin Heimer <heimer16@gmail.com>
2013-08-23 16:11:55 -0600

    escape literal dots in url regexes

--- background.js
+++ background.js
@@ -12,31 +12,31 @@
 
 var configurations = {
       amazon : {
-        rx: /^http.*?\.amazon.com.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
+        rx: /^http.*?\.amazon\.com.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i,
         params: [
           { param: "tag", paramValue: "thelinactsho-20" }
         ]
       },
       amazonuk : {
-       rx: /^http.*?\.amazon.co.uk.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
+       rx: /^http.*?\.amazon\.co\.uk.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i,
        params: [
          { param: "tag", paramValue: "jupitebroadc-21" }
        ]
       },
       amazonde : { 
-        rx: /^http.*?\.amazon.de.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, 
+        rx: /^http.*?\.amazon\.de.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i, 
         params: [
           { param: "tag", paramValue: "jupitebroad02-21" }
         ]
       },
       amazonca : { 
-        rx: /^http.*?\.amazon.ca.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
+        rx: /^http.*?\.amazon\.ca.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i,
         params: [
           { param: "tag", paramValue: "jbcanada-20" }
         ]
       },
       audible : { 
-        rx: /^http.*?\.audible.com/i, 
+        rx: /^http.*?\.audible\.com/i, 
         params: [
           { param: "source_code", paramValue: "COMA0230WS012110" },
           { param: "AID", paramValue: "10298646" },
@@ -44,7 +44,7 @@ var configurations = {
         ]
       },
       mint : { 
-        rx: /^http.*?\.mint.com/i, 
+        rx: /^http.*?\.mint\.com/i, 
         params: [
           { param: "PID", paramValue: "4897915" },
           { param: "priorityCode", paramValue: "4216102399" },
@@ -52,7 +52,7 @@ var configurations = {
         ]
       },
       bestbuy : {
-       rx: /^http.*?\.bestbuy.com.site.*?\?id=/i, 
+       rx: /^http.*?\.bestbuy\.com\.site.*?\?id=/i, 
        params: [
          { param: "AID", paramValue: "10483113" },
          { param: "PID", paramValue: "4897915" },
@@ -62,7 +62,7 @@ var configurations = {
        ]
       },
       thinkgeek : { 
-        rx: /^http.*?\.thinkgeek.com/i, 
+        rx: /^http.*?\.thinkgeek\.com/i, 
         params: [
           { param: "cpg", paramValue: "cj" },
           { param: "ref", paramValue: "" },
@@ -71,7 +71,7 @@ var configurations = {
         ]
       },
       neweggcom : {
-       rx: /^http.*?\.newegg.com.(product.product.aspx\?item=|special.shellshocker.aspx\?)/i,
+       rx: /^http.*?\.newegg\.com\.(product\.product\.aspx\?item=|special\.shellshocker\.aspx\?)/i,
        params: [
          { param: "nm_mc", paramValue: "AFC-C8Junction" },
          { param: "cm_mmc", paramValue: "AFC-C8Junction-_-Branding-_-na-_-na" },
@@ -80,7 +80,7 @@ var configurations = {
        ]
       },
       neweggca : {
-       rx: /^http.*?\.newegg.ca.(product.product.aspx\?item=|special.shellshocker.aspx\?)/i,
+       rx: /^http.*?\.newegg\.ca\.(product\.product\.aspx\?item=|special\.shellshocker\.aspx\?)/i,
        params: [
          { param: "nm_mc", paramValue: "AFC-C8junctionCA" },
          { param: "cm_mmc", paramValue: "AFC-C8JunctionCA-_-homepage-_-na-_-na" },
@@ -89,7 +89,7 @@ var configurations = {
        ]
       },
       guitarcenter : {
-        rx: /^http.*?\.guitarcenter.com/i, 
+        rx: /^http.*?\.guitarcenter\.com/i, 
         params: [
           { param: "CJAID", paramValue: "10453836" },
           { param: "CJPID", paramValue: "4897915" }
