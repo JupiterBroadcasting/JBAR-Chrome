6434bd180ccd073688f77591c80ed42a008d14be
Richard Korman <winter.blood@gmail.com>
2013-09-09 05:09:54 -0400

    Revert overly restrictive URL matching
    Should have tested/looked @ MR #13 closer, broke newegg, parts of amazon
    and best buy affiliates.

--- background.js
+++ background.js
@@ -12,25 +12,25 @@
 
 var configurations = {
       amazon : {
-        rx: /^http.*?\.amazon\.com.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i,
+        rx: /^http.*?\.amazon\.com.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
         params: [
           { param: "tag", paramValue: "thelinactsho-20" }
         ]
       },
       amazonuk : {
-       rx: /^http.*?\.amazon\.co\.uk.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i,
+       rx: /^http.*?\.amazon\.co\.uk.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
        params: [
          { param: "tag", paramValue: "jupitebroadc-21" }
        ]
       },
       amazonde : { 
-        rx: /^http.*?\.amazon\.de.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i, 
+        rx: /^http.*?\.amazon\.de.*?(\/dp\/|obidos.tg.detail|.gp.product)/i, 
         params: [
           { param: "tag", paramValue: "jupitebroad02-21" }
         ]
       },
       amazonca : { 
-        rx: /^http.*?\.amazon\.ca.*?(\/dp\/|obidos\.tg\.detail|\.gp\.product)/i,
+        rx: /^http.*?\.amazon\.ca.*?(\/dp\/|obidos.tg.detail|.gp.product)/i,
         params: [
           { param: "tag", paramValue: "jbcanada-20" }
         ]
@@ -52,7 +52,7 @@ var configurations = {
         ]
       },
       bestbuy : {
-       rx: /^http.*?\.bestbuy\.com\.site.*?\?id=/i, 
+       rx: /^http.*?\.bestbuy\.com.*?site.*?\?id=/i, 
        params: [
          { param: "AID", paramValue: "10483113" },
          { param: "PID", paramValue: "4897915" },
@@ -71,7 +71,7 @@ var configurations = {
         ]
       },
       neweggcom : {
-       rx: /^http.*?\.newegg\.com\.(product\.product\.aspx\?item=|special\.shellshocker\.aspx\?)/i,
+       rx: /^http.*?\.newegg\.com.*?(product.product\.aspx\?item=|special.shellshocker\.aspx\?)/i,
        params: [
          { param: "nm_mc", paramValue: "AFC-C8Junction" },
          { param: "cm_mmc", paramValue: "AFC-C8Junction-_-Branding-_-na-_-na" },
@@ -80,7 +80,7 @@ var configurations = {
        ]
       },
       neweggca : {
-       rx: /^http.*?\.newegg\.ca\.(product\.product\.aspx\?item=|special\.shellshocker\.aspx\?)/i,
+       rx: /^http.*?\.newegg\.ca.*?\/(product.product\.aspx\?item=|special.shellshocker\.aspx\?)/i,
        params: [
          { param: "nm_mc", paramValue: "AFC-C8junctionCA" },
          { param: "cm_mmc", paramValue: "AFC-C8JunctionCA-_-homepage-_-na-_-na" },
